254dc425089ddb2a20ce8349f495d3067a54cbbb
morostami <P9172369905r>
2020-07-05 00:26:48 +0430

    sprint 4 - final cut

--- app/views/search.py
+++ app/views/search.py
@@ -19,7 +19,25 @@ class AdvertisementListSearch(generics.ListCreateAPIView):
         if query is not None:
             queryset = queryset.filter(title__icontains=query) | \
                        queryset.filter(description__icontains=query)
-        return queryset
+        return queryset.filter(active=True)
+
+    def get(self, request, *args, **kwargs):
+        response: Response = super().get(request, *args, **kwargs)
+        response.content_type = 'application/json; charset=utf-8'
+
+        return response
+
+
+class AdvertisementListSearchByCategory(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = AdvertisementSerializer
+
+    def get_queryset(self):
+        queryset = Advertisement.objects.all()
+        cat_id = self.request.query_params.get('category_id', None)
+        if cat_id is not None:
+            queryset = queryset.filter(category_id=cat_id)
+        return queryset.filter(active=True)
 
     def get(self, request, *args, **kwargs):
         response: Response = super().get(request, *args, **kwargs)
